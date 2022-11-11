d17b6a341cdd12d198942e7f3f1b12760baa84ed
William Luck <luck.william.h@gmail.com>
2022-11-11 11:48:28 -0300

    commit tree 3e76f99bcab30e2c1034ca1b6d7af7843e81cf5e

--- app/controllers/members_only_articles_controller.rb
+++ app/controllers/members_only_articles_controller.rb
@@ -1,5 +1,6 @@
 class MembersOnlyArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :authorize
 
   def index
     articles = Article.where(is_member_only: true).includes(:user).order(created_at: :desc)
@@ -12,6 +13,12 @@ class MembersOnlyArticlesController < ApplicationController
   end
 
   private
+
+  def authorize
+    if !session[:user_id]
+    render json: {error: 'Not authorized'}, status: :unauthorized
+    end
+  end
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
